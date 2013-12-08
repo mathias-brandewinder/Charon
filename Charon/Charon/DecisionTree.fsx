c6bf535e51a9ab24844b74474e9a315a5a43d126
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-12-08 02:26:16 -0800

    Fixed bug in tree test script

--- Charon/Charon/DecisionTree.fsx
+++ Charon/Charon/DecisionTree.fsx
@@ -11,12 +11,18 @@ open System.IO
 open System.Diagnostics
 
 // Create a synthetic, random dataset and run a tree on it
-let test (size: int) (feat: int) (outcomes: int) =
+let test (size: int) (feat: int) (outcomes: int) isNumeric =
 
     let rng = System.Random()
 
-    let labels = [| for i in 0 .. size -> rng.Next(0, outcomes + 1) |] 
-    let data = [| for f in 1 .. feat -> [| for i in 0 .. size -> rng.Next(0, outcomes + 1) |] |> Discrete.prepare |> Categorical |]
+    let labels = [| for i in 0 .. size -> rng.Next(0, outcomes) |] 
+    let data = 
+        if isNumeric then
+            [| for f in 1 .. feat -> 
+                [| for i in 0 .. size -> Some(rng.NextDouble()), labels.[i] |] |> Numeric |]
+        else
+            [| for f in 1 .. feat -> 
+                [| for i in 0 .. size -> rng.Next(0, outcomes) |] |> Discrete.prepare |> Categorical |]
 
     let dataset = { Classes = feat; Outcomes = labels; Features = data }
 
